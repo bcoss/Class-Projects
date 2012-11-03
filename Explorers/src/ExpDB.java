19284dd50b83117405275ffabf2cbae1ee069e61
jcoss <b.coss90@gmail.com>
2012-11-03 19:08:48 -0400

    7:08 Nov. 3

--- Explorers/src/ExpDB.java
+++ Explorers/src/ExpDB.java
@@ -4,59 +4,99 @@
  * @author  Jeff Pittges
  * @version Jun-2011
  */
-import java.io.*;
-import java.sql.*;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class ExpDB implements ExpDataSource 
-{
-	Connection conn = null; 
+public class ExpDB implements ExpDataSource {
+	Connection conn = null;
+	public String nickname, username;
+	public int EID;
+	Explorer exp;
 
-    /**
-     * getExplorer() -- retrieve the explorer 
-     *
-	 * @param username -- name of the player who controls this explorer
-     * @returns - An explorer, null if not found.
-     */
-	public Explorer getExplorer(String username)
-	{
-		Explorer exp = null;
+	public ExpDB() throws SQLException {
+		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+
+		String user, pass, query;
+		user = "jcoss";
+		pass = "nikebial";
+		query = "select * from explorer";
+
+		try {
+			// Connection conn =
+			// DriverManager.getConnection("jdbc:oracle:thin:@Picard.radford.edu:1521:teaching",
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@Picard2.radford.edu:1521:itec2", user,
+					pass);
+			Statement stmt = conn.createStatement();
+			ResultSet rset = stmt.executeQuery(query);
+			while (rset.next()) {
+				System.out.println(rset.getString("EID") + "  "
+						+ rset.getString("name") + "  "
+						+ rset.getString("username"));
+
+				this.EID = rset.getInt("EID");
+				this.nickname = rset.getString("name");
+				this.username = rset.getString("username");
+
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Could not load the db" + e);
+		}
+	}
+
+	/**
+	 * getExplorer() -- retrieve the explorer
+	 * 
+	 * @param username
+	 *            -- name of the player who controls this explorer
+	 * @returns - An explorer, null if not found.
+	 */
+	public Explorer getExplorer(String s) {
+
+		exp = new Explorer(EID, nickname, username, 0);
+		System.out.println(exp.toString());
 
 		return exp;
 	}
-    
-	/**
-     * getRoomDescr() -- returns a description of the given room 
-     *
-     * @param roomNum - number of the target room. 
-     * @returns A string describing the given room, empty string if not found. 
-     */
-    public String getRoomDescr(int roomNum)
-	{
-		String roomDescr = ""; 
-
-		return roomDescr; 
-	}
 
 	/**
-     * move() -- move the given explorer to the next room.
-     *  
-     * @param exp -- the explorer to be moved 
-     */
-    public void move(Explorer exp)
-	{
-	}
-
-	/**
-	 * close() -- performs any cleanup before the program terminates. 
+	 * getRoomDescr() -- returns a description of the given room
+	 * 
+	 * @param roomNum
+	 *            - number of the target room.
+	 * @returns A string describing the given room, empty string if not found.
 	 */
-	public void close()
-	{
+	public String getRoomDescr(int roomNum) {
+		String roomDescr = "";
+
+		return roomDescr;
 	}
 
-	public void test()
-	{
+	/**
+	 * move() -- move the given explorer to the next room.
+	 * 
+	 * @param exp
+	 *            -- the explorer to be moved
+	 */
+	public void move(Explorer exp) {
+	}
+
+	/**
+	 * close() -- performs any cleanup before the program terminates.
+	 */
+	public void close() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void test() {
 	}
 }
-
-
--- Explorers/src/ExpDriver.java
+++ Explorers/src/ExpDriver.java
@@ -4,18 +4,18 @@
  * @author  Jeff Pittges
  * @version Jun-2011
  */
+import java.sql.SQLException;
 import java.util.Scanner;
-public class ExpDriver
-{
-	public static void main(String[] args)
-	{
-		final int END_ROOM = -1; 		// designates an end room 
 
-		ExpDataSource ds = new ExpFile();
-		//ExpDataSource ds = new ExpDB();
+public class ExpDriver {
+	public static void main(String[] args) throws SQLException {
+		final int END_ROOM = -1; // designates an end room
+
+		// ExpDataSource ds = new ExpFile();
+		ExpDataSource ds = new ExpDB();
 
 		// Test the data source
-		ds.test(); 
+		ds.test();
 
 		/********************/
 		/* Get the Explorer */
@@ -25,32 +25,29 @@ public class ExpDriver
 		// prompt for the player's username
 		System.out.print("\nEnter username: ");
 		String username = scan.next();
-		
+
 		Explorer exp = ds.getExplorer(username);
 
-		// describe the explorer 
+		// describe the explorer
 		if (exp != null)
-		   System.out.println("\n" + exp);
+			System.out.println("\n" + exp);
 		else
-		   System.out.println("\nExplorer for " + username + " not found\n");
+			System.out.println("\nExplorer for " + username + " not found\n");
 
 		/*****************/
 		/* Play the game */
 		/*****************/
 
-		// move through all of the rooms 
-		while (exp.getRoomNum() != END_ROOM)
-		{
+		// move through all of the rooms
+		while (exp.getRoomNum() != END_ROOM) {
 			// describe the explorer's room
 			System.out.println(ds.getRoomDescr(exp.getRoomNum()));
 
-			// move the explorer to the next room 
+			// move the explorer to the next room
 			ds.move(exp);
 		}
 
-		// close the data source 
-		ds.close(); 
+		// close the data source
+		ds.close();
 	}
 }
-
-
